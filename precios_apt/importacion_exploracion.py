78c8f02d0a90716d5ba78ce5f3e8932d8625728e
Hdez_J <jlopezhdez036@gmail.com>
2025-11-02 22:36:37 +1000

    updating

--- precios_apt/importacion_exploracion.py
+++ precios_apt/importacion_exploracion.py
@@ -33,7 +33,7 @@ df.shape # (43013, 46)
 df.columns
 
 df.info()
-df.isna().sum()
+df.isna().sum() # No muestra datos vacios, pero no es así
 
 
 # Vector de salida [Sólo estudiaremos precios de venta, no renta]
@@ -52,29 +52,28 @@ df.administracion = pd.to_numeric(df.administracion, errors= 'coerce')
 df.parqueaderos = pd.to_numeric(df.parqueaderos, errors= 'coerce')
 df.estrato = pd.to_numeric(df.estrato, errors= 'coerce')
 
-
 df.shape #(27583, 46)
 
 # Columnas de fechas
 
-df['datetime'] = pd.to_datetime(df['datetime'])
+# df['datetime'] = pd.to_datetime(df['datetime'])
 
-df.datetime.iloc[30853:30858]
-df.datetime.iloc[30855] = df.datetime.iloc[30854]
+# df.datetime.iloc[30853:30858]
+# df.datetime.iloc[30855] = df.datetime.iloc[30854]
 
-df.datetime.iloc[38780:38783]
-df.datetime.iloc[38781] = df.datetime.iloc[38780]
+# df.datetime.iloc[38780:38783]
+# df.datetime.iloc[38781] = df.datetime.iloc[38780]
 
-df['datetime'] = pd.to_datetime(df['datetime'])
+# df['datetime'] = pd.to_datetime(df['datetime'])
 
 
-df['last_view'] = pd.to_datetime(df['last_view'])
+# df['last_view'] = pd.to_datetime(df['last_view'])
 
-df.last_view.iloc[30853:30857]
-df.last_view.iloc[30855] = df.last_view.iloc[30854]
-df.last_view.iloc[38781] = df.last_view.iloc[38780]
+# df.last_view.iloc[30853:30857]
+# df.last_view.iloc[30855] = df.last_view.iloc[30854]
+# df.last_view.iloc[38781] = df.last_view.iloc[38780]
 
-df['last_view'] = pd.to_datetime(df['last_view'])
+# df['last_view'] = pd.to_datetime(df['last_view'])
 
 
 #%% Análisis exploratorio (EDA)
@@ -87,7 +86,6 @@ df['log_precio'] = np.log(df.precio_venta)
 df['log_precio'].describe()
 
 sns.set_style("whitegrid")
-
 
 fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize=(8, 4))
 ax[0].hist(df.precio_venta, bins = 'sturges', density = True)
@@ -122,7 +120,7 @@ def  Tukey_method(x, k = 1.5):
 
 
 out, lower, upper = Tukey_method(df.precio_venta, k=1.5)
-len(out)
+len(out) # 1945
 
 
 # Eliminemos los outliers, ¿por qué?
@@ -132,9 +130,10 @@ df = df[(df.precio_venta > lower) & (df.precio_venta < upper)]
 plt.figure(figsize= (9,4))
 sns.kdeplot(data =df, x = 'precio_venta')
 
+# Dada la muestra, solamente reviaremos apartamentos
+
 df['tipo_propiedad'].value_counts()
 
-# Dada la muestra, solamente reviaremos apartamentos
 df = df[df.tipo_propiedad == 'APARTAMENTO']
 
 plt.figure(figsize= (9,4))
@@ -148,6 +147,7 @@ localidades = df.localidad.value_counts()
 
 len(localidades) # 19 localidades
 
+# Apartamentos por localidad
 plt.figure(figsize= (9,4))
 sns.barplot(x = localidades.index, y = localidades.values,
             palette= 'viridis')
@@ -157,6 +157,8 @@ plt.ylabel('Frecuencia')
 plt.xticks(rotation = 50, fontsize = 8)
 plt.tight_layout()
 plt.show()
+
+# Distribución por localidad
 
 plt.figure(figsize= (9,4))
 sns.boxplot(data = df, y = 'precio_venta', x = 'localidad', 
@@ -169,12 +171,161 @@ plt.tight_layout()
 plt.show()
 
 
+df_mean = df.groupby('localidad')['precio_venta'].mean().rename('Media')
+df_median = df.groupby('localidad')['precio_venta'].median().rename('Mediana')
 
-df.groupby('localidad')['precio_venta'].mean()
-
-df.columns
-
-X = df['tipo_propiedad']
+pd.concat([df_mean, df_median], axis=1)
 
 
-df['tipo_propiedad'].value_counts()
+# Distribución por estrato
+
+plt.figure(figsize= (9,4))
+sns.boxplot(data = df, y = 'precio_venta', x = 'estrato', 
+            palette= 'viridis')
+plt.title('Precio de apartamentos por localidad')
+plt.xlabel('Localidades')
+plt.ylabel('Precio de venta')
+plt.xticks(rotation = 50, fontsize = 8)
+plt.tight_layout()
+plt.show()
+
+# Caso inusual del estrato 0
+
+df.estrato.value_counts() # 4 en estrato 0
+df[df.estrato == 0.0].localidad # Es ruido innecesario
+
+# Procedemos a eliminarlo
+df = df[df.estrato != 0.0]
+
+# Area
+
+out, lower, upper = Tukey_method(df.area, k = 1.5)
+len(out)
+
+# Restrinjamos el area
+df = df[(df.area > lower) & (df.area < upper)]
+
+plt.figure(figsize=(12, 5))
+sns.scatterplot(
+    data= df,
+    y='precio_venta',
+    x= 'area',
+    #hue = 'is_cerca_parque', #'is_cerca_estacion_tm',
+    alpha=.8,
+    color='steelblue',
+)
+
+plt.title('Precio de venta vs área de apartamentos')
+plt.ylabel('Precio de venta')
+plt.xlabel('Área (m2)')
+plt.show()
+
+
+# Apartamentos cerca a parques
+
+plt.figure(figsize=(6, 6))
+
+df['is_cerca_parque'].value_counts().plot.pie(
+    autopct='%1.1f%%',
+    startangle=90,
+    colors=['#ff9999','#66b3ff'],
+    explode=(0.1, 0),
+    labels=['Lejos (>500m)', 'Cerca (≤500m)'],
+)
+
+plt.title('Proximidad a parques en Bogotá')
+plt.ylabel('')
+plt.xlabel('')
+plt.show()
+
+# Localidade sy parques
+
+plt.figure(figsize=(12, 5))
+
+sns.barplot(
+    y=df.groupby('localidad')['is_cerca_parque'].sum().sort_values(ascending=False).index,
+    x=df.groupby('localidad')['is_cerca_parque'].sum().sort_values(ascending=False),
+)
+
+plt.title('Cantidad de apartamentos cerca de parques en Bogotá por localidad (<= 500m)')
+plt.xlabel('Cantidad de apartamentos')
+plt.ylabel('Localidad')
+
+# Cercania a estaciones de TM
+
+plt.figure(figsize=(12, 5))
+
+sns.kdeplot(
+    data=df,
+    x='precio_venta',
+    hue='is_cerca_estacion_tm',
+    bw_adjust=.5,
+    hue_order=[True, False],
+    alpha=.8,
+)
+
+plt.title('Distribución de precios de venta de apartamentos en Bogotá por cercanía a estación de TM (<= 500m)')
+plt.xlabel('Precio de venta')
+plt.show()
+
+
+# Función de Distribución Acumulativa Empírica (ECDF)
+plt.figure(figsize=(12, 5))
+
+sns.ecdfplot(
+    data = df,
+    x='precio_venta',
+    hue='is_cerca_estacion_tm',
+    hue_order=[True, False],
+    alpha=.8,
+)
+
+plt.title('ECDF de precios de venta de apartamentos en Bogotá por cercanía a estación de TM (<= 500m)')
+plt.xlabel('Precio de venta')
+plt.show()
+
+# Errores en parqueadero
+df.parqueaderos.value_counts()
+
+df = df[(df.parqueaderos > 0) & (df.parqueaderos < 5)]
+
+
+
+#%% Especificación del modelo
+
+# Reduciré las localidades a 4 zonas
+
+condiciones = [
+    df['localidad'].isin(['USAQUEN', 'CHAPINERO', 'SUBA', 'BARRIOS UNIDOS']),
+    df['localidad'].isin(['FONTIBON', 'ENGATIVA', 'KENNEDY', 'BOSA', 'PUENTE ARANDA']),
+    df['localidad'].isin(['TEUSAQUILLO', 'SANTA FE', 'CANDELARIA', 'LOS MARTIRES', 'ANTONIO NARINO', 'SAN CRISTOBAL']),
+    df['localidad'].isin(['CIUDAD BOLIVAR', 'USME', 'RAFAEL URIBE URIBE', 'TUNJUELITO'])
+]
+
+zonas = ['NORTE', 'OCCIDENTE', 'CENTRO/ORIENTE', 'SUR']
+
+df['zona_bogota'] = np.select(
+    condiciones, 
+    zonas, default='OTRA')
+
+# Con 'jacuzzi',  'gimnasio', 'piscina' haré una variable binaria
+
+lista = ['jacuzzi',  'gimnasio', 'piscina']
+
+for i in lista:
+    df[i] = pd.to_numeric(df[i], errors= 'coerce')
+
+# Luego se aplica la condición (vectorizada) para crear la columna binaria
+df['deportivas'] = ( df['jacuzzi'] + df['gimnasio'] + df['piscina'] > 0).astype(int)
+
+
+y = df['precio_venta']
+
+X = df[['area', 'habitaciones','banos', 'administracion','parqueaderos',
+       'estrato', 'conjunto_cerrado', 'vigilancia','distancia_estacion_tm_m',
+       'distancia_parque_m', 'zona_bogota', 'deportivas','antiguedad']]
+
+
+X.dropna(inplace = True)
+
+X.isna().sum()
